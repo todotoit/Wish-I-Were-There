0b7d0da00d0bccb83c940fccdf957587048b3a55
Emilio <e.bondioli@todo.to.it>
2020-04-08 16:05:12 +0200

    replace vuefire with vuexfire

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { vuexfireMutations, firestoreAction } from 'vuexfire'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 Vue.use(Vuex)
 
+
+const db = firebase.initializeApp({
+  apiKey: "AIzaSyCp7iVOxs-Glz355NA_FHG0LZBrnsyaOMA",
+  authDomain: "mappette-2f68a.firebaseapp.com",
+  databaseURL: "https://mappette-2f68a.firebaseio.com",
+  projectId: "mappette-2f68a",
+  storageBucket: "mappette-2f68a.appspot.com",
+  messagingSenderId: "31611820265",
+  appId: "1:31611820265:web:bb2d5f1a16927a6153f878"
+}).firestore()
+
 export default new Vuex.Store({
+  // setup the reactive todos property
   state: {
+    origins: [],
   },
-  mutations: {
-  },
+
+  mutations: vuexfireMutations,
+
   actions: {
+    bindTodosRef: firestoreAction(context => {
+      // context contains all original properties like commit, state, etc
+      // and adds `bindFirestoreRef` and `unbindFirestoreRef`
+      // we return the promise returned by `bindFirestoreRef` that will
+      // resolve once data is ready
+      return context.bindFirestoreRef('origins', db.collection('origins'))
+    }),
   },
-  modules: {
-  }
 })
