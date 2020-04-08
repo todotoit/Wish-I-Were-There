73aeb19f041cf2061de554038e3e7a95dfd4435b
Emilio <e.bondioli@todo.to.it>
2020-04-08 13:43:33 +0200

    temporarily hide ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home panel">
     <h2>Mappette</h2>
-    <p>Intro al progetto > Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus lectus lorem magnis gravida. Purus aliquam tempor, penatibus vitae nibh accumsan congue nunc. Rhoncus vel vel morbi enim, nibh sed proin amet, ullamcorper. Enim diam vel vestibulum massa adipiscing arcu sit. At interdum nunc ipsum a. Ultrices eget fermentum, sit elit sed magna. Nibh sagittis odio nunc libero. Imperdiet tristique malesuada nisi, ultricies sagittis mattis. Mi aliquam pulvinar turpis ut porttitor semper ut.</p>
+    <p>Intro</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home"
 };
