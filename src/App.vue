af0de99c8dc01442aa84c3b184bc817b73c3f55f
Emilio <e.bondioli@todo.to.it>
2020-04-09 12:37:27 +0200

    reset route when reloading page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <div class="overlay center">
-      <router-view v-show="false" />
+    <div class="overlay center" v-show="$route.path !== '/explore'">
+      <a @click="$router.push('/explore')" class="close">CLOSE</a>
+      <router-view />
     </div>
     <Map :api-key="mapsKey" />
   </div>
@@ -17,6 +18,7 @@ export default {
     };
   },
   mounted() {
+    if(this.$route.path !== '/') this.$router.push('/')
     Promise.all([
       this.$store.dispatch("bindUsersRef"),
       this.$store.dispatch("bindPinsRef")
@@ -32,12 +34,17 @@ body {
   height: 100%;
   margin: 0;
   padding: 0;
+  background-color: #212121;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #ffffff;
   height: 100%;
+}
+.close {
+  cursor: pointer;
+  pointer-events: all;
 }
 </style>
