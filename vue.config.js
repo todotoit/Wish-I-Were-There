279e999426a4d106152f18e0e638a39c955c726d
Kerem <kerem.turkyilmaz@outlook.it>
2020-04-09 11:41:35 +0200

    add svg inline loader

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-svg-inline-loader")
+      .loader("vue-svg-inline-loader")
+      .options({ /* ... */ });
+    config.resolve.alias.set('assets', path.resolve('src/assets'));
+  }
 }
