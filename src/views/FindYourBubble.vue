ebbd7c999fe5582771ad1119f1cc052ceb4b9729
Emilio <e.bondioli@todo.to.it>
2020-04-09 12:38:10 +0200

    add bubble place view

---
+++ src/views/FindYourBubble.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home panel">
+    <h2>Mappette</h2>
+    <p>Place the pin in the location where you are spending this time of self-quarantine</p>
+    <MarkerPlacer v-if="ready" />
+    <button @click="createNewBubble()">Place your bubble</button>
+  </div>
+</template>
+
+<script>
+import MarkerPlacer from "@/components/MarkerPlacer.vue";
+
+export default {
+  name: "FindYourBubble",
+  components: { MarkerPlacer },
+  computed: {
+    ready() {
+      return this.$store.state.ready;
+    },
+    marker() {
+      return this.$store.state.marker
+    }
+  },
+  methods: {
+    createNewBubble() {
+      this.$store.dispatch('createNewUser', this.marker).then(r => console.log(r))
+    }
+  }
+};
+</script>
