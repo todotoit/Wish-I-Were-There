262681e64fa89e43919792de25acc8aca5a89c95
Kerem <kerem.turkyilmaz@outlook.it>
2020-04-09 12:00:53 +0200

    create info component

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,23 @@
       <router-view v-show="false" />
     </div>
     <Map :api-key="mapsKey" />
+    <div class="btn-info" @click="info = !info">
+      <div class="open" v-if="!info" />
+      <div class="close" v-else />
+    </div>
+    <Info v-if="info" />
   </div>
 </template>
 
 <script>
 import Map from "@/components/Map/Map";
+import Info from "@/components/Info";
 export default {
   name: "App",
   data() {
     return {
       mapsKey: process.env.VUE_APP_MAPS_KEY,
+      info: false
     };
   },
   mounted() {
@@ -22,7 +29,7 @@ export default {
       this.$store.dispatch("bindPinsRef")
     ]);
   },
-  components: { Map }
+  components: { Map, Info }
 };
 </script>
 
@@ -39,5 +46,25 @@ body {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   height: 100%;
+}
+
+.btn-info {
+  position: fixed;
+  z-index: 101;
+  top: 0;
+  right: 0;
+  padding: 0.5rem;
+  width: 40px;
+  height: 40px;
+  .open {
+    width: 100%;
+    height: 100%;
+    background-color: red;
+  }
+  .close {
+    width: 100%;
+    height: 100%;
+    background-color: blue;
+  }
 }
 </style>
