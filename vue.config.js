ab604c26b0e947d8887efe1a137c93d99922368f
Kerem <kerem.turkyilmaz@outlook.it>
2020-04-10 10:29:25 +0200

    add global scss variables

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,19 @@ const path = require('path');
 
 module.exports = {
   lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~@/assets/css/variables.scss";`
+      }
+    }
+  },
   chainWebpack: config => {
     config.module
       .rule("vue")
       .use("vue-svg-inline-loader")
       .loader("vue-svg-inline-loader")
-      .options({ });
+      .options({});
     config.resolve.alias.set('assets', path.resolve('src/assets'));
   },
 }
