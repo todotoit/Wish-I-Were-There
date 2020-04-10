89231da058e8c784542bc34bdcbd8cd8c1daa7c7
Emilio <e.bondioli@todo.to.it>
2020-04-10 15:13:40 +0200

    include vue-cookie

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueCookie from 'vue-cookie';
 import './assets/css/style.scss'
+
+Vue.use(VueCookie);
 
 Vue.config.productionTip = false
 
