e13e08940f263171b9f0dc63e551da5077faaadb
Emilio <e.bondioli@todo.to.it>
2020-04-10 14:16:47 +0200

    create cover component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home panel">
+  <div class="home">
     <h2>Mappette</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit congue dolor faucibus varius. Nam vel urna augue. Duis et accumsan quam. Nunc scelerisque tincidunt felis. Fusce vel facilisis ex, ut scelerisque nulla. Nunc sagittis, arcu in posuere suscipit, neque erat scelerisque tellus, elementum posuere nisi mi sit amet turpis. Proin sagittis iaculis urna ut hendrerit. Sed tempus id metus at lacinia. Ut id tortor vel metus rutrum volutpat.</p>
     <p>
@@ -13,3 +13,12 @@ export default {
   name: "Home"
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background-color: #1D1B26;
+  width: 100%;
+  height: 100%;
+  pointer-events: all;
+}
+</style>
