2467398e9ba0603ea60850cc7ae2883f09cf9ed4
Kerem <kerem.turkyilmaz@outlook.it>
2020-04-15 12:20:44 +0200

    add cookie step

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="home">
-    <div class="view">
+    <div class="view" v-if="step == 0">
       <div class="info">
         <div class="header center">
           <p class="expa-large">{{ $t('title') }}</p>
           <p class="exte-large">{{ $t('intro') }}</p>
         </div>
         <div class="footer">
-          <button @click="$router.push('/bubble')">{{ $t('ctaStart') }}</button>
+          <button @click="step = 1">{{ $t('ctaStart') }}</button>
           <p class="exte-small link" @click="$router.push('/explore')">{{ $t('phase03Skip') }}</p>
+        </div>
+      </div>
+      <LanguageSwitch />
+    </div>
+    <div class="view" v-else>
+      <div class="info">
+        <div class="header center">
+          <img svg-inline class="logo" src="@/assets/icons/cookie.svg" />
+          <p class="exte-large">{{ $t('cookieDesc') }}</p>
+        </div>
+        <div class="footer">
+          <button @click="$router.push('/bubble')">{{ $t('cookieBtn') }}</button>
         </div>
       </div>
       <LanguageSwitch />
@@ -23,7 +35,12 @@ import LanguageSwitch from "@/components/LanguageSwitch";
 
 export default {
   name: "Home",
-  components: { Cover, LanguageSwitch }
+  components: { Cover, LanguageSwitch },
+  data() {
+    return {
+      step: 0
+    };
+  }
 };
 </script>
 
@@ -35,6 +52,14 @@ export default {
   background-color: #1d1b26;
   .view {
     pointer-events: all;
+    .header {
+      align-items: center;
+      svg {
+        outline: none;
+        user-select: none;
+        margin-bottom: 2rem;
+      }
+    }
   }
 }
 </style>
