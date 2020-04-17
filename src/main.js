5e5d1bf4d1e86b7f85419a44933cb23acda56b05
Emilio <e.bondioli@todo.to.it>
2020-04-17 12:48:55 +0200

    install vue-mq

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import VueCookie from 'vue-cookie';
 import VueSocialSharing from 'vue-social-sharing';
 import VueI18n from 'vue-i18n'
 import localization from './localization.js'
+import VueMq from 'vue-mq'
 
 import './assets/css/style.scss'
 import './assets/css/transitions.scss'
@@ -14,6 +15,15 @@ import './assets/fonts/stylesheet.css'
 Vue.use(VueSocialSharing);
 Vue.use(VueCookie);
 Vue.use(VueI18n)
+Vue.use(VueMq, {
+  breakpoints: {
+    xsm: 450,
+    sm: 600,
+    md: 1200,
+    lg: Infinity,
+  }
+})
+
 
 const i18n = new VueI18n({
   locale: 'it',
