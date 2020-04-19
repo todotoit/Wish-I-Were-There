fc36dfbc181580944d58e0d9f15ea712146a82b0
Emilio <e.bondioli@todo.to.it>
2020-04-19 18:05:33 +0200

    manage labels separately from bubbles

--- src/components/Map/bubble/bubble-overlay.js
+++ src/components/Map/bubble/bubble-overlay.js
@@ -17,15 +17,16 @@ export function createBubble(map, center, options) {
 
 /** @constructor */
 function BubbleOverlay(bounds, map, options) {
-    this.bounds_ = bounds;
-    this.map_ = map;
-    this.user_ = options.user
+    this.bounds = bounds;
+    this.map = map;
+    this.user = options.user
+    this.marker = options.marker
     this.images = [
         require('@/assets/img/bubbles/bubbles-gradient.png'),
         require('@/assets/img/bubbles/bubbles-expanded.png')
     ]
 
-    this.div_ = null;
+    this.div = null;
     this.setMap(map);
 }
 
@@ -34,64 +35,69 @@ function createOverlayProto() {
 
     BubbleOverlay.prototype.onAdd = function () {
         const div = document.createElement('div');
-        const label = document.createElement('label');
-        div.appendChild(label)
-        label.classList.add('exte-small');
-        label.innerText = this.user_.name
+        this.label = document.createElement('label');
+        this.label.classList.add('exte-small', 'bubble-label');
+        this.label.innerText = this.user.name
         div.classList.add('bubble-container');
-        this.bubble_ = new BubbleImage(div, this.images)
-        this.div_ = div;
-        /*         this.div_.style.top = '0'
-                this.div_.style.left = '0' */
-        //this.div_.style.transformOrigin = 'top left'
+        this.bubble = new BubbleImage(div, this.images)
+        this.div = div;
         const panes = this.getPanes();
         panes.overlayLayer.appendChild(div);
+        panes.overlayLayer.appendChild(this.label)
         google.maps.event.addDomListener(div, 'click', e => {
             google.maps.event.trigger(this.marker, 'click');
         });
-        const d = this.map_.selectedUserMarker && !(this.map_.selectedUserMarker.user.id === this.user_.id)
-        this.setVisible(this.map_.showUserMarkers)
+        const d = this.map.selectedUserMarker && !(this.map.selectedUserMarker.user.id === this.user.id)
+        this.setVisible(this.map.showUserMarkers)
         this.setDisabled(d)
     };
 
     BubbleOverlay.prototype.draw = function () {
-        //if (this.debounce) return
         this.update()
     };
 
     BubbleOverlay.prototype.update = function () {
-        //this.debounce = true
-        //setTimeout(() => this.debounce = false, 12)
+
         const overlayProjection = this.getProjection();
         const zoom = overlayProjection['T'].zoom
         let zoomLevel = 0
         if (zoom > 16) zoomLevel = 1
 
-        const sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-        const ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+        const markerPos = overlayProjection.fromLatLngToDivPixel(this.marker.getPosition())
+        this.label.style.top = markerPos.y + 'px'
+        this.label.style.left = markerPos.x + 'px'
+
+        const sw = overlayProjection.fromLatLngToDivPixel(this.bounds.getSouthWest());
+        const ne = overlayProjection.fromLatLngToDivPixel(this.bounds.getNorthEast());
 
         if (zoom < 14) {
-            this.div_.classList.add('far')
-            if(!this.div_.classList.contains('force-visible')) return
+            this.div.classList.add('far')
+            this.label.classList.add('far')
+            if (!this.div.classList.contains('force-visible')) return
         }
-        else this.div_.classList.remove('far')
+        else {
+            this.div.classList.remove('far')
+            this.label.classList.remove('far')
+        }
 
-        //this.div_.style.transform = 'translate(' + ne.y + 'px, ' + sw.x + 'px)';
-        this.div_.style.left = sw.x + 'px';
-        this.div_.style.top = ne.y + 'px';
-        this.div_.style.width = (ne.x - sw.x) + 'px';
-        this.div_.style.height = (sw.y - ne.y) + 'px';
-        this.bubble_.setZoomLevel(zoomLevel)
+        this.div.style.left = sw.x + 'px';
+        this.div.style.top = ne.y + 'px';
+        this.div.style.width = (ne.x - sw.x) + 'px';
+        this.div.style.height = (sw.y - ne.y) + 'px';
+        this.bubble.setZoomLevel(zoomLevel)
     };
 
     BubbleOverlay.prototype.setVisible = function (val) {
-        if (this.div_) this.div_.classList.toggle('hidden', !val)
+        if (this.div) this.div.classList.toggle('hidden', !val)
+        if (this.label) this.label.classList.toggle('hidden', !val)
     }
     BubbleOverlay.prototype.setDisabled = function (val) {
-        if (this.div_) this.div_.classList.toggle('disabled', !!val)
+        if (this.div) this.div.classList.toggle('disabled', !!val)
+        if (this.label) this.label.classList.toggle('disabled', !!val)
     }
     BubbleOverlay.prototype.forceVisible = function (val) {
-        if (this.div_) this.div_.classList.toggle('force-visible', !!val)
+        if (this.div) this.div.classList.toggle('force-visible', !!val)
+        if (this.label) this.label.classList.toggle('force-visible', !!val)
     }
 }
 
