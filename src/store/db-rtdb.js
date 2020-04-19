f963c55174b875fc62e58d44d2adaf65ba29e6d9
Emilio <e.bondioli@todo.to.it>
2020-04-19 15:01:59 +0200

    add real time update to rtdb module

--- src/store/db-rtdb.js
+++ src/store/db-rtdb.js
@@ -1,32 +1,59 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 import 'firebase/auth'
-import { firebaseProdConfig } from './config.js'
+import { firebaseDevConfig } from './config.js'
 
-export const db = firebase.initializeApp(firebaseProdConfig).database()
+export const db = firebase.initializeApp(firebaseDevConfig).database()
+const usersRef = db.ref('users')
+const pinsRef = db.ref('pins')
+
 firebase.auth().signInWithEmailAndPassword(process.env.VUE_APP_FIREBASE_USER, process.env.VUE_APP_FIREBASE_PASS)
 
+function prepareUser(context, snap) {
+    const user = snap.val()
+    user.id = snap.key
+    return user
+}
+function preparePin(context, snap) {
+    const pin = snap.val()
+    pin.id = snap.key
+    pin.user = context.getters.getUser(pin.user)
+    return pin
+}
+
 export const store = {
-    mutations: {},
+    mutations: {
+        ADD_USER(state, data) {
+            state.users = [...state.users, data]
+        },
+        ADD_PIN(state, data) {
+            state.pins = [...state.pins, data]
+        }
+    },
     actions: {
         getUsers: context => {
-            return db.ref('users').once('value', snapshot => {
+            usersRef.on('child_added', function (data) {
+                const user = prepareUser(context, data)
+                context.commit('ADD_USER', user)
+            });
+            return usersRef.once('value', snapshot => {
                 const users = []
                 snapshot.forEach(s => {
-                    const user = s.val()
-                    user.id = s.key
+                    const user = prepareUser(context, s)
                     users.push(user)
                 });
                 context.commit('SET_USERS', users)
             })
         },
         getPins: context => {
-            return db.ref('pins').once('value', snapshot => {
+            pinsRef.on('child_added', function (data) {
+                const pin = preparePin(context, data)
+                context.commit('ADD_PIN', pin)
+            });
+            return pinsRef.once('value', snapshot => {
                 const pins = []
                 snapshot.forEach(s => {
-                    const pin = s.val()
-                    pin.id = s.key
-                    pin.user = context.getters.getUser(pin.user)
+                    const pin = preparePin(context, s)
                     pins.push(pin)
                 });
                 context.commit('SET_PINS', pins)
@@ -36,7 +63,7 @@ export const store = {
             const coordinates = data.marker.getPosition()
             const id = context.getters.getUniqueUserId()
             const userData = {
-                coordinates: {Wa: coordinates.lat(), za: coordinates.lng()},
+                coordinates: { Wa: coordinates.lat(), za: coordinates.lng() },
                 created: new Date().toISOString(),
                 name: data.name
             }
@@ -48,14 +75,13 @@ export const store = {
                         id,
                         ...userData,
                     }
-                    context.commit('SET_USERS', [...context.state.users, user])
                     return { ...user, marker: data.marker }
                 })
         },
         createNewPin: (context, data) => {
             const coordinates = data.marker.getPosition()
             const pinData = {
-                coordinates: {Wa: coordinates.lat(), za: coordinates.lng()},
+                coordinates: { Wa: coordinates.lat(), za: coordinates.lng() },
                 created: new Date().toISOString(),
                 user: data.user.id,
                 message: data.message
@@ -63,9 +89,8 @@ export const store = {
             return db.ref('pins').push(pinData).then(r => {
                 const pin = {
                     ...pinData,
-                    id: r.id,
+                    id: r.key,
                 }
-                context.commit('SET_PINS', [...context.state.pins, pin])
                 return { pin, marker: data.marker }
             })
         }
