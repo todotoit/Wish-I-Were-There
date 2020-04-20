846151419bc36881058c45b720ab1ec568820280
Emilio <e.bondioli@todo.to.it>
2020-04-20 14:49:08 +0200

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    ['@vue/cli-plugin-babel/preset', {
+      useBuiltIns: 'usage',
+      polyfills: [
+        'es.object.assign',
+        'es.object.values',
+        'es.array'
+      ]
+    }]
   ]
 }
