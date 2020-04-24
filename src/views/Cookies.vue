148ddd72279609fa9b491b7eaa0b95a7c6b7ae39
Emilio <e.bondioli@todo.to.it>
2020-04-24 18:58:36 +0200

    update privacy policy copy

--- src/views/Cookies.vue
+++ src/views/Cookies.vue
@@ -3,8 +3,8 @@
     <div class="view-content">
       <header>
         <img svg-inline class="logo" src="@/assets/icons/cookie.svg" />
-        <p class="exte-large">{{ $t('cookieDesc') }}</p>
-        <a href="/privacy" target="_blank">{{ $t('privacyLink') }}</a>
+        <p class="exte-large" v-html="$t('cookieDesc')"></p>
+        <a href="/privacy" class="privacy-policy" target="_blank">{{ $t('privacyLink') }}</a>
       </header>
       <footer>
         <button @click="$router.push('/bubble')">{{ $t('cookieBtn') }}</button>
@@ -30,8 +30,14 @@ export default {
     svg {
       margin-bottom: 1rem;
     }
+    a.privacy-policy {
+      display: block;
+      margin-bottom: 0.5rem;
+      color: $col-green;
+      margin-top: 1rem;
+    }
   }
-  a {
+  p {
     margin-bottom: 0.5rem;
   }
 }
--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -1,12 +1,30 @@
 <template>
   <section class="privacy">
-    <div class="content" v-html="$t('privacyPolicy')">
+    <LanguageSwitch class="language-switch" />
+    <header>
+      <p class="exte-small project-name" @click="$router.push('/')">Wish I Were There</p>
+    </header>
+    <div class="content">
+      <div class="intro">
+        <h3>{{ $t('privacyIntroTitle') }}</h3>
+        <div v-html="$t('privacyIntroText')"></div>
+        <h3>{{ $t('privacyHowItWorksTitle') }}</h3>
+        <div v-html="$t('privacyHowItWorksText')"></div>
+      </div>
+      <h2 class="expa-large">Privacy policy</h2>
+      <div class="privacy-policy" v-html="$t('privacyPolicy')"></div>
+      <h2 class="expa-large">Cookie policy</h2>
+      <div class="privacy-policy" v-html="$t('cookiePolicy')"></div>
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import LanguageSwitch from "@/components/LanguageSwitch";
+
+export default {
+  components: { LanguageSwitch }
+};
 </script>
 
 <style lang="scss">
@@ -16,11 +34,46 @@ section.privacy {
   overflow: auto;
   width: 100%;
   pointer-events: all;
+  background-color: $col-dark;
+  h2 {
+    text-align: center;
+    margin-top: 2rem;
+  }
+  h3 {
+    text-align: center;
+    color: $col-green;
+  }
+  h4 {
+    text-align: center;
+    margin-top: 0;
+  }
+  .language-switch {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+  }
+  header {
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    margin-bottom: 3rem;
+  }
+  .intro {
+    text-align: center;
+  }
+  .project-name {
+    cursor: pointer;
+    text-transform: uppercase;
+    color: $col-green;
+  }
   a {
     text-transform: none;
+    color: $col-green;
   }
   .content {
-    max-width: 960px;
+    max-width: 840px;
     margin: 0 auto;
     p,
     li {
