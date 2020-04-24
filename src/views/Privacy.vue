dd3c2de28a805de34ad97952a06edbf800792c20
Emilio <e.bondioli@todo.to.it>
2020-04-24 18:18:17 +0200

    create privacy page

---
+++ src/views/Privacy.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="privacy">
+    <div class="content" v-html="$t('privacyPolicy')">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+section.privacy {
+  text-align: left;
+  padding: 1rem;
+  overflow: auto;
+  width: 100%;
+  pointer-events: all;
+  a {
+    text-transform: none;
+  }
+  .content {
+    max-width: 960px;
+    margin: 0 auto;
+    p,
+    li {
+      margin: 0.8rem 0;
+    }
+  }
+}
+</style>
